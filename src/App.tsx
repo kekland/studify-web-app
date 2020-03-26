cdeef2156a604e89e62df7949aa02a7a53f0440d
Erzhan <kk.erzhan@gmail.com>
2020-03-26 19:09:19 +0600

    wip: refactor some things

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import './theme.css'
-import './index.css'
+import './design/theme.css'
+import './design/index.css'
+import './design/design.css'
 import { MainPage } from './pages/main-page/main-page'
+import { AuthPage } from './pages/auth-page/auth-page';
 
 function App() {
   return (
     <div className='root body-light body'>
-      <MainPage />
+      <AuthPage />
     </div>
   );
 }
---
+++ src/pages/auth-page/auth-page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AuthPage: React.FC = (props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
