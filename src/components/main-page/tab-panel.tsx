694c4f857f31154af9432c9b432e4f4536d9a18a
Erzhan <kk.erzhan@gmail.com>
2020-03-27 20:32:20 +0600

    impl: custom scrollbar

--- src/components/main-page/tab-panel.tsx
+++ src/components/main-page/tab-panel.tsx
@@ -5,8 +5,10 @@ import { Column, Flexible } from '../flex/flex'
 import { GroupHorizontal } from '../group-component/group-component'
 import { ListItemButton } from '../list-item-button/list-item-button'
 import { faPlus, faSearch } from '@fortawesome/free-solid-svg-icons'
-import { AutoSizer, List } from 'react-virtualized'
 import { selectGroup } from '../../state/main'
+import { FixedSizeList } from 'react-window'
+import { AutoSizer } from 'react-virtualized'
+import { CustomScrollbarsVirtualList } from '../smart-list/smart-list'
 
 export interface ITabPanelProps {
   onCreateNewGroup: () => void;
@@ -14,25 +16,30 @@ export interface ITabPanelProps {
 
 export const TabPanel: React.FC<ITabPanelProps> = (props) => {
   const groups = useSelector((state: RootState) => state.main.groups)
+
   return (
     <Column mainAxisSize='max' crossAxisSize='max'>
       <Flexible style={{ width: '100%' }}>
         <AutoSizer>
           {({ height, width }) => (
-            <List
-              rowCount={groups.length}
-              rowHeight={80}
+            <FixedSizeList
+              outerElementType={CustomScrollbarsVirtualList}
+              itemCount={groups.length}
+              itemSize={80}
               width={width}
               height={height}
-              rowRenderer={props =>
-                <GroupHorizontal
-                  key={props.key}
-                  group={groups[props.index]}
-                  selected={false}
-                  padding='12px'
-                  onTap={() => store.dispatch(selectGroup(groups[props.index]))} />
+            >
+              {({ style, index }) =>
+                <div
+                  style={style}>
+                  <GroupHorizontal
+                    group={groups[index]}
+                    selected={false}
+                    padding='12px'
+                    onTap={() => store.dispatch(selectGroup(groups[index]))} />
+                </div>
               }
-            />
+            </FixedSizeList>
           )}
         </AutoSizer>
       </Flexible>
---
+++ src/components/smart-list/smart-list.tsx
@@ -0,0 +1,27 @@
+import React, { useCallback } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
+
+const CustomScrollbars: React.FC<any> = ({ onScroll, forwardedRef, style, children }) => {
+  const refSetter = useCallback(scrollbarsRef => {
+    if (scrollbarsRef) {
+      forwardedRef(scrollbarsRef.view);
+    } else {
+      forwardedRef(null);
+    }
+  }, []);
+
+  return (
+    <Scrollbars
+      ref={refSetter}
+      autoHide
+      style={{ ...style, overflow: "hidden" }}
+      onScroll={onScroll}
+    >
+      {children}
+    </Scrollbars>
+  );
+};
+
+export const CustomScrollbarsVirtualList = React.forwardRef((props, ref) => (
+  <CustomScrollbars {...props} forwardedRef={ref} />
+));
