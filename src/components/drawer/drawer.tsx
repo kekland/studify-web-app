e4031f6e6df751e98bcb2f371893ad5ffe466115
Erzhan <kk.erzhan@gmail.com>
2020-03-30 12:31:37 +0600

    fix: drawer onclick

--- src/components/drawer/drawer.tsx
+++ src/components/drawer/drawer.tsx
@@ -19,8 +19,8 @@ export const Drawer: React.FC<IDrawerProps> = ({ isOpen, width, children, onClos
         bottom: 0,
         right: 0,
         zIndex: 999,
-        pointerEvents: isOpen ? 'all' : 'none',
-      }} onClick={onClose}>
+        pointerEvents: 'none',
+      }}>
       <div style={{
         width: '100vw',
         height: '100%',
@@ -33,19 +33,21 @@ export const Drawer: React.FC<IDrawerProps> = ({ isOpen, width, children, onClos
         opacity: isOpen ? 1 : 0,
         transition: 'opacity 250ms ease-in-out',
         zIndex: 999,
-      }} />
-      <div style={{
-        position: 'absolute',
-        left: onLeft ? 0 : `calc(100vw - ${width})`,
-        top: 0,
-        bottom: 0,
-        right: onLeft ? `calc(100vw - ${width})` : 0,
-        backgroundColor: 'var(--color-surface)',
-        zIndex: 1000,
-        transform: isOpen ? 'translate(0, 0)' : `translate(${onLeft ? '-' : ''}${width}, 0)`,
-        transition: 'transform 250ms ease-in-out',
-      }}>
-        {children}
+        pointerEvents: isOpen ? 'auto' : 'none',
+      }} onClick={onClose}>
+        <div style={{
+          position: 'absolute',
+          left: onLeft ? 0 : `calc(100vw - ${width})`,
+          top: 0,
+          bottom: 0,
+          right: onLeft ? `calc(100vw - ${width})` : 0,
+          backgroundColor: 'var(--color-surface)',
+          zIndex: 1000,
+          transform: isOpen ? 'translate(0, 0)' : `translate(${onLeft ? '-' : ''}${width}, 0)`,
+          transition: 'transform 250ms ease-in-out',
+        }} onClick={(e) => e.stopPropagation()}>
+          {children}
+        </div>
       </div>
     </div>
   )
